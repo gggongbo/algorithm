a349c44e5c1df7148b9478f129ceaa84a90d9aad
gggongbo <rudqh9494@gmail.com>
2023-12-12 18:57:17 +0900

    Time: 121 ms (5.08%) | Memory: 54.9 MB (5.23%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    public int numIslands(char[][] grid) {
+        int answer = 0;
+
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[i].length; j++){
+                if(grid[i][j] == '1'){
+                    checkIsland(grid, i, j);
+                    System.out.println("checkStart" + i + j);
+                    answer ++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public void checkIsland(char[][] grid,  int checkY, int checkX) {
+        grid[checkY][checkX] = '0';
+        if(checkY-1 > -1 && grid[checkY-1][checkX] == '1'){
+            System.out.println("up ok" + (checkY-1) + checkX);
+            checkIsland(grid, checkY-1, checkX);
+        }
+        if(checkY+1 < grid.length && grid[checkY+1][checkX] == '1'){
+            System.out.println("down ok" + (checkY+1) + checkX);
+            checkIsland(grid, checkY+1, checkX);
+        }
+        if(checkX-1 > -1 && grid[checkY][checkX-1] == '1'){
+            System.out.println("left ok" + checkY + (checkX-1));
+            checkIsland(grid, checkY, checkX-1);
+        }
+        if(checkX+1 < grid[checkY].length && grid[checkY][checkX+1] == '1'){
+            System.out.println("right ok" + checkY + (checkX+1));
+            checkIsland(grid, checkY, checkX+1);
+        }
+    }
+}
