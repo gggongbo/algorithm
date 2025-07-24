34120bf63207731a03e836b4750633f6d0e4714a
gggongbo <rudqh9494@gmail.com>
2025-07-24 22:52:44 +0900

    [level 1] Title: 모의고사, Time: 6.13 ms, Memory: 92.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        int[][] pattern = {
+            {1,2,3,4,5},
+            {2,1,2,3,2,4,2,5},
+            {3,3,1,1,2,2,4,4,5,5}
+        };
+        
+        int [] scores = new int[3];
+        
+        for(int i=0; i< answers.length; i++){
+            for(int j=0; j<pattern.length; j++){
+                if(answers[i] == pattern[j][i%pattern[j].length]){
+                    scores[j]++;
+                }
+            }
+        }
+        
+        int maxScore = Arrays.stream(scores).max().getAsInt();
+        
+        ArrayList<Integer> answerList = new ArrayList<>();
+        for(int i=0; i< scores.length; i++){
+            if (scores[i] == maxScore){
+                answerList.add(i+1);
+            }
+        }
+        
+        return answerList.stream().sorted().mapToInt(a->a).toArray();
+    }
+}
