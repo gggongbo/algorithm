a26162482bd277efd3107aac74e05c7ad6060457
gggongbo <rudqh9494@gmail.com>
2025-07-24 21:28:01 +0900

    [set활용답안] 두 개 뽑아서 더하기.java

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -3,16 +3,16 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] numbers) {
         int[] answer = {};
-        List<Integer> sumList = new ArrayList<Integer>();
+        HashSet<Integer> set = new HashSet<>();
 
         for (int i = 0; i < numbers.length - 1; i++) {
             for (int j = i + 1; j < numbers.length; j++) {
-                sumList.add(numbers[i] + numbers[j]);
+                set.add(numbers[i] + numbers[j]);
             }
         }
         
-        answer = sumList.stream().distinct()
-                .sorted().mapToInt(a -> a).toArray();
+        answer = set.stream()
+                .sorted().mapToInt(Integer::intValue).toArray();
         
         return answer;
     }
