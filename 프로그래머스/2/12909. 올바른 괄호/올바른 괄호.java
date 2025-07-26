efd3b153ec036004cc97a49acee3fe3a9201fc02
gggongbo <rudqh9494@gmail.com>
2025-07-27 00:52:27 +0900

    [책 답안 정리] 올바른 괄호.java

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -11,10 +11,10 @@ class Solution {
             if(c == '('){
                 stack.push(c);
             } else {
-                if(stack.isEmpty()){
+                //stack.pop() == c인 경우가 생각이 안나지만, 안전장치로 추가
+                if(stack.isEmpty() || stack.pop() == c){
                     return false;
                 }
-                stack.pop();
             }
             
         }
